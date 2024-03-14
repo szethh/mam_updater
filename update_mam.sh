a5c443b8f1dc99e58dec36169a08eab1cadfe977
szethh <33635766+szethh@users.noreply.github.com>
2024-03-14 23:12:26 +0100

    some changes and more logging

--- update_mam.sh
+++ update_mam.sh
@@ -3,12 +3,19 @@
 # adapted from https://github.com/qdm12/gluetun/issues/1785#issuecomment-1944451745
 
 # store the last known public IP address
-ip_file="./public_ip.txt"
+ip_file="/data/ip"
 current_ip=$(curl -s ipinfo.io/ip)
+
+echo "[$(date '+%Y-%m-%d %H:%M:%S')] MAM updater starting. IP=$current_ip"
 
 # Check if the IP has changed
 if [ ! -f "$ip_file" ] || [ "$(cat "$ip_file")" != "$current_ip" ]; then
     echo "$current_ip" > "$ip_file"
+
+    echo "IP changed since last run. Updating MAM..."
     
-    curl -c /data/mam.cookies -b /data/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php
+    response = $(curl -c /data/mam.cookies -b /data/mam.cookies https://t.myanonamouse.net/json/dynamicSeedbox.php)
+    echo "$response"
+else
+    echo "IP is unchanged: $current_ip. No action taken"
 fi
